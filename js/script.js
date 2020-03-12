dc16c2c6b57998029f13f638a744317790418e91
Erim Bayramoglu <54535933+ErimAlp@users.noreply.github.com>
2020-03-12 14:28:34 +0100

    Klar med sidan
    klar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("hejsan");
+function showHide(id) {
+    var x = document.getElementById(id);
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+var x = document.getElementById("text_10");
+x.style.display = "none";
+
+var x = document.getElementById("text_00");
+x.style.display = "none";
+
+var x = document.getElementById("text_90");
+x.style.display = "none";
+
+var x = document.getElementById("text_80");
+x.style.display = "none";
+
+var x = document.getElementById("text_70");
+x.style.display = "none";
+
+var x = document.getElementById("text_60");
+x.style.display = "none";
+
